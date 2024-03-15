13cf4226e080bb49591943af77dcad18d38d6e90
Tejasw <tejasw.789@gmail.com>
2024-03-15 18:46:30 +0530

    added few changes

--- client/src/AuthModal.js
+++ client/src/AuthModal.js
@@ -10,7 +10,8 @@ function AuthModal(){
   const [password,setPassword] = useState('');
     return(
         <div className="w-screen h-screen fixed top-0 left-0 z-30 flex" style={{backgroundColor:'rgba(0,0,0,.6)'}}>
-            <div className="border border-gray-200  w-3/4 sm:w-1/2 lg:w-1/4 bg-black p-5 text-reddit_text self-center mx-auto rounded-md">
+            <div className="border border-gray-200  w-3/4 sm:w-1/2 lg:w-1/4 bg-black p-5 text-reddit_text 
+            self-center mx-auto rounded-md">
                 {/* <h1 className='text-white text-2xl mb-3'>Login</h1> */}
                 {modalType === 'login' && (
             <h1 className="text-white text-2xl mb-5">Login</h1>
