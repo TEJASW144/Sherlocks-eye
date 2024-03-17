6fa58a9d6df21cd1b6ebc647d04aa33b3659ff58
arpitgami <arpit.20225104@mnnit.ac.in>
2024-03-17 14:18:37 +0530

    Changes in BoardHead

--- client/src/BoardHeader.js
+++ client/src/BoardHeader.js
@@ -10,12 +10,15 @@ function BoardHeader() {
       ></div>
       <div className="bg-gray-700">
         <div className="mx-4 relative flex">
+
           <div className="h-20 w-20 rounded-full overflow-hidden relative -top-4 border border-white">
+            
             <img src="https://styles.redditmedia.com/t5_2qs0q/styles/communityIcon_kxcmzy9bt1381.jpg?width=256&format=pjpg&s=0a2e472f6fae0712fee4a3b5d44920fe35dbcdaa" />
+          
           </div>
           <div className="pt-2 pl-4">
             <h1 className="text-gray-300 text-3xl ">
-              Sherlock's Eye: Welcome to Complete Anonymity
+              Sherlock's Eye: Welcome to Complete Anonymity...
             </h1>
             <h5 className="text-gray-500">WebApp</h5>
           </div>
