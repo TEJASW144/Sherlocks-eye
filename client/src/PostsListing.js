99d5ce99683e72a3e882e42866365cc7dd26e9f2
itsmead <123891355+aditya-gup780@users.noreply.github.com>
2024-03-17 13:42:51 +0530

    commentserrorsolved

--- client/src/PostsListing.js
+++ client/src/PostsListing.js
@@ -1,27 +1,27 @@
 import Post from "./Post";
 import { useState, useEffect } from "react";
 import axios from "axios";
-function PostListing(props){
-    const [comments, setComments] = useState([]);
-    useEffect(()=>{
-        axios
+function PostsListing() {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    axios
       .get("http://localhost:4000/comments", { withCredentials: true })
-      .then((response) => setComments(response.data))
+      .then((response) => {
+        console.log(response.data);
+        setComments(response.data);
+      })
 
       .catch((error) => {
         // Handle error here
         console.error("Error fetching user data:", error);
-        
       });
-    }, []);
-     
-    return(
-        <div className="bg-gray-950">
-            {props.comments.map(comment=>(
-              <Post {...comment}/>
-            ))}
-          </div>
-    );
+  }, []);
+  return (
+    <div className="bg-gray-950">
+      {comments.map((comment) => (
+        <Post {...comment} />
+      ))}
+    </div>
+  );
 }
-
-export default PostListing;
+export default PostsListing;
